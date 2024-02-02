c5cce7324d8d6c52516e2592a730575d98ce98e2
Aldo Parada <41877271+Aldo1803@users.noreply.github.com>
2024-02-02 17:22:51 -0600

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 async function connectToDatabase() {
     const connection = await mysql.createConnection({
-        host: 'database-1.ciaq66skd7qn.us-east-2.rds.amazonaws.com',
+        host: 'YOUR-DB-DIRECTION',
         user: 'admin',
         password: 'admin123', // Replace with your MySQL password
         database: 'log_database',
