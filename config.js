f84c532921467d7abd2f1fd7fbc0a1bf1987f45d
Aldo Parada <41877271+Aldo1803@users.noreply.github.com>
2024-02-02 17:22:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    awsAccesskeyID: "AKIA5F6S2C5NAQNGCUOJ",
-    awsSecretAccessKey: "aomRpyCbsFXi2gu/yIvTWZ96nJif5WI6vaHs7wOX",
+    awsAccesskeyID: "YOUR-ACCESS-KEY",
+    awsSecretAccessKey: "YOUR-SECRET-ACCESS-KEY",
     awsRegion: "us-east-2",
     secret: "secret"
 };
